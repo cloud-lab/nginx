72da81d37f2b22766c51774f529182f184072b46
jonowych <jonowych@users.noreply.github.com>
2020-02-28 15:45:23 +1100

    add IP address in port mapping

--- web-built.sh
+++ web-built.sh
@@ -1,3 +1,3 @@
-docker run --name nginx-1 -v /home/lnxadmin/nginx/pencil.html:/usr/share/nginx/html/index.html -p 2180:80 -d nginx
-docker run --name nginx-2 -v /home/lnxadmin/nginx/screen.html:/usr/share/nginx/html/index.html -p 2280:80 -d nginx
-
+# prepare Ubuntu VM with two IP addresses in second interface
+docker run --name nginx-1 -v /home/lnxadmin/nginx/pencil.html:/usr/share/nginx/html/index.html -p 192.168.56.16:2180:80 -d nginx
+docker run --name nginx-2 -v /home/lnxadmin/nginx/screen.html:/usr/share/nginx/html/index.html -p 192.168.56.17:2280:80 -d nginx
